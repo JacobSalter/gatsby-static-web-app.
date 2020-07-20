6f6da4a0b2bc028f259fee054ea3b59620cd72ee
Jacob Salter <jacob.salter@i-finity.co.uk>
2020-07-20 09:29:18 +0100

    test buld through to azure

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,6 +12,7 @@ const IndexPage = () => (
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <p>new added thing.</p>
+    <p>wowza more things?!?!?.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
